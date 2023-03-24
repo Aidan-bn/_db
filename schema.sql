ea5d0763205e33c0bd3b9e64cbb97f6d5e1600d7
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2023-03-24 23:47:50 +0300

    create animal database,  schema.sql, query.sql, and data.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE DATABASE vet_clinic;
 
-CREATE
+CREATE TABLE animals (
+    id int,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg float
+);
